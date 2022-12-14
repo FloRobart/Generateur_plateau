96d87f30f78fb4ee1bd01d2fa0244c711e98e25f
Anaelle Mimifir <anaellemimi27@gmail.com>
2022-12-14 14:36:11 +0100

    update ihm

--- src/controleur/Controleur.java
+++ src/controleur/Controleur.java
@@ -3,23 +3,23 @@ import java.awt.Color;
 import java.io.File;
 
 import ihm.FramePlateau;
-import metier.Metier;
+//import metier.Metier;
 
 public class Controleur
 {
 	private FramePlateau ihm;
-	private Metier       metier;
+	//private Metier       metier;
 
 	public Controleur()
 	{
 		this.ihm     = new FramePlateau(this);
-		this.metier  = null;
+		//this.metier  = null;
 	}
 
 	//methodes
 	public void nouveau() 
     {
-		this.metier = new Metier();
+		//this.metier = new Metier();
 		
         this.ihm.dispose();
         this.ihm = new FramePlateau(this);
@@ -27,27 +27,23 @@ public class Controleur
 
     public void ouvrir(File fichier) 
     {
-		this.metier = new Metier(fichier);
-		System.out.println(this.metier);
+		//this.metier = new Metier(fichier);
+		//System.out.println(this.metier);
 		/*
         this.ihm.dispose();
         this.ihm = new FrameCuves(this);*/
     }
 
     public void enregistrer    () { this.ihm.enregistrer (); }
-	public void enregistrerSous    () { this.ihm.enregistrer (); }
-    public void exporterSous() { this.ihm.exporterSous(); }
-    public void frameDispose() { this.ihm.dispose     (); }
+	public void enregistrerSous() { this.ihm.enregistrer (); }
+    public void exporterSous   () { this.ihm.exporterSous(); }
+    public void frameDispose   () { this.ihm.dispose     (); }
 
 
 	public void genererTxt(String type, String nomFichier)
 	{
 		this.ihm.dispose();
 	}
-
-	//accesseurs
-
-	public static void main(String[] args) { new Controleur(); }
 
     public void setCouleur(Color color) {
 
@@ -63,5 +59,12 @@ public class Controleur
 			default:
 				break;
 		}
-	}   
+	}
+
+
+	/**
+	 * Main
+	 * @param args : Tableau de String
+	 */
+	public static void main(String[] args) { new Controleur(); }
 }
