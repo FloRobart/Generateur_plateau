f392d6740c911f51ae1dc15bf2c1d2f8505c1d2a
Lucas Lemarchand <95851237+LucasLemarch@users.noreply.github.com>
2022-12-14 09:58:06 +0100

    Debut XML informations générales

--- src/metier/Metier.java
+++ src/metier/Metier.java
@@ -1,24 +1,19 @@
-package metier;
-
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Base64;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.imageio.ImageIO;
 
-import org.jdom2.Document;
-import org.jdom2.Element;
-import org.jdom2.input.SAXBuilder;
-import org.jdom2.output.Format;
-import org.jdom2.output.XMLOutputter;
+import org.jdom2.*;
+import org.jdom2.input.*;
 
 public class Metier 
 {
@@ -34,206 +29,112 @@ public class Metier
 	private BufferedImage imageCarte;
 	private List<Color>   couleurs;
 
-	private List<CarteObjectif> piocheCarteObjectif;
+	private List<CarteObjectif> carteObjectif;
 	private List<Noeud>         noeuds;
 	private List<Arete>         aretes;
 	
-	public Metier()
+	public Metier(String nomFichier)
 	{
-		this.taillePlateau       = new int[2];
-		this.piocheCarteObjectif = new ArrayList<CarteObjectif>();
-		this.noeuds              = new ArrayList<Noeud>();
-		this.aretes              = new ArrayList<Arete>();
+		this.taillePlateau = new int[2];
+		this.carteObjectif = new ArrayList<CarteObjectif>();
+		this.noeuds        = new ArrayList<Noeud>();
+		this.aretes        = new ArrayList<Arete>();
 
-		this.lireFichier("nomFichier");
+		this.lireFichier(nomFichier);
 	}
 
-	public int[]               getTaillePlateau      () { return this.taillePlateau;       }
-	public BufferedImage       getImagePlateau       () { return this.imagePlateau;        }
-	public Color               getCouleurPlateau     () { return this.couleurPlateau;      }
-	public Font                getPolicePlateau      () { return this.policePlateau;       }
-	public int                 getNbJoueursMin       () { return this.nbJoueursMin;        }
-	public int                 getNbJoueursMax       () { return this.nbJoueursMax;        }
-	public int                 getNbCarteCoul        () { return this.nbCarteCoul;         }
-	public int                 getNbCarteLocomotive  () { return this.nbCarteLocomotive;   }
-	public BufferedImage       getImageCarte         () { return this.imageCarte;          }
-	public List<Color>         getCouleurs           () { return this.couleurs;            }
-	public List<CarteObjectif> getPiocheCarteObjectif() { return this.piocheCarteObjectif; }
-	public List<Noeud>         getNoeuds             () { return this.noeuds;              }
-	public List<Arete>         getAretes             () { return this.aretes;              }
-
-	/*
-	 * <?xml version="1.0" encoding="UTF-8"?>
-<jeu>
-	<information>
-		<dimension x="500" y="300"/>
-		<image-fond>image_encoder_en_base64</image-fond>
-		<couleur-fond>0x000000</couleur-fond>
-		<police taille="12">police</police>
-		<nombre-joueurs min="2" max="10"/>
-		<nombre-carte couleur="12" multicouleur="15"/>
-		<image-carte>image_encoder_en_base64</image-carte>
-	</information>
-	<plateau>
-		<liste-noeuds>
-			<noeud id="1">
-				<position x="100" y="100"/>
-				<nom>noeud1</nom>
-				<position-nom x="120" y="120"/>
-				<couleur>0x0000FF</couleur>
-			</noeud>
-			<noeud id="2">
-				<position x="300" y="100"/>
-				<nom>noeud2</nom>
-				<position-nom x="320" y="120"/>
-				<couleur>0xFF0000</couleur>
-			</noeud>
-		</liste-noeuds>
-		<liste-aretes>
-			<arret>
-				<noeud n1="1" n2="2"/>
-				<couleur>0x00FF00</couleur>
-				<distance>3</distance>
-				<liste-troncons>
-					<troncon>
-						
-					</troncon>
-					<troncon>
-						
-					</troncon>
-					<troncon>
-						
-					</troncon>
-				</liste-troncons>
-			</arret>
-		</liste-aretes>
-		<liste-objectifs>
-			<objectif>
-				<noeud n1="1" n2="2"/>
-				<points>3</points>
-			</objectif>
-		</liste-objectifs>
-	</plateau>
-</jeu>
-	 */
+	public int[]               getTaillePlateau    () { return this.taillePlateau;     }
+	public BufferedImage       getImagePlateau     () { return this.imagePlateau;      }
+	public Color               getCouleurPlateau   () { return this.couleurPlateau;    }
+	public Font                getPolicePlateau    () { return this.policePlateau;     }
+	public int                 getNbJoueursMin     () { return this.nbJoueursMin;      }
+	public int                 getNbJoueursMax     () { return this.nbJoueursMax;      }
+	public int                 getNbCarteCoul      () { return this.nbCarteCoul;       }
+	public int                 getNbCarteLocomotive() { return this.nbCarteLocomotive; }
+	public BufferedImage       getImageCarte       () { return this.imageCarte;        }
+	public List<Color>         getCouleurs         () { return this.couleurs;          }
+	public List<CarteObjectif> getCarteObjectif    () { return this.carteObjectif;     }
+	public List<Noeud>         getNoeuds           () { return this.noeuds;            }
+	public List<Arete>         getAretes           () { return this.aretes;            }
 
 	private void lireFichier(String nomFichier)
 	{
-		File fichier = new File(nomFichier);
-		Document document;
+		String lienFichier = "./" + nomFichier + ".xml";
+
+		SAXBuilder sxb = new SAXBuilder();
 		try {
-			document = new SAXBuilder().build(fichier);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return;
-		}
+			Document document = sxb.build(new File( lienFichier ));
 
-		// lire information
-		Element information = document.getRootElement().getChild("information");
-		this.nbJoueursMax = Integer.parseInt(information.getChild("nombre-joueurs").getAttributeValue("max"));
-		this.nbJoueursMin = Integer.parseInt(information.getChild("nombre-joueurs").getAttributeValue("min"));
+			/* <jeu> */
+			Element racine = document.getRootElement();
 
-		String image_carte_base64 = information.getChild("image-carte").getText();
-		byte[] image_carte = Base64.getDecoder().decode(image_carte_base64);
-		try {
-			this.imageCarte = ImageIO.read(new ByteArrayInputStream(image_carte));
-		} catch (IOException e) {
-			this.imageCarte = new BufferedImage(400, 400, BufferedImage.TYPE_INT_RGB);
-			this.imageCarte.getGraphics().drawString("Image carte non trouvée", 20, 20);
-		}
+			/* <information> */
+			Element information = racine.getChild("information");
 
-		String image_plateau_base64 = information.getChild("image-fond").getText();
-		byte[] image_plateau = Base64.getDecoder().decode(image_plateau_base64);
-		try {
-			this.imagePlateau = ImageIO.read(new ByteArrayInputStream(image_plateau));
-		} catch (IOException e) {
-			this.imagePlateau = new BufferedImage(400, 400, BufferedImage.TYPE_INT_RGB);
-			this.imagePlateau.getGraphics().drawString("Image plateau non trouvée", 20, 20);
-		}
+			Element dimension = information.getChild("dimension");
+			this.taillePlateau[0] = Integer.parseInt(dimension.getAttributeValue("x"));
+			this.taillePlateau[1] = Integer.parseInt(dimension.getAttributeValue("y"));
+			this.imagePlateau     = this.base64ToImage(information.getChild("image-fond").getText()); 
+			this.couleurPlateau   = this.hexaToColor(information.getChild("couleur-fond").getText());   
+			this.policePlateau    = new Font (information.getChild("police").getText(), Font.PLAIN, 12);
+			
+			Element nbJoueurs = information.getChild("nombre-joueurs");
+			this.nbJoueursMin = Integer.parseInt(nbJoueurs.getAttributeValue("min"));
+			this.nbJoueursMax = Integer.parseInt(nbJoueurs.getAttributeValue("max"));
 
-		this.couleurPlateau = new Color(Integer.parseInt(information.getChild("couleur-fond").getText(), 16));
-		String font_name = information.getChild("police").getText();
-		int font_size = Integer.parseInt(information.getChild("police").getAttributeValue("taille"));
-		this.policePlateau = new Font(font_name, Font.PLAIN, font_size);
+			Element nbCarte        = information.getChild("nombre-carte");
+			this.nbCarteCoul       = Integer.parseInt(nbCarte.getAttributeValue("couleur"));
+			this.nbCarteLocomotive = Integer.parseInt(nbCarte.getAttributeValue("multicouleur"));
+			this.imageCarte        = this.base64ToImage(information.getChild("image-carte").getText());  
 
-		this.nbCarteCoul = Integer.parseInt(information.getChild("nombre-carte").getAttributeValue("couleur"));
-
+		} catch (Exception e){}
 	}
-	private void genererFichier(String nomFichier)
+
+	private String imageToBase64(BufferedImage image) throws IOException {
+        ByteArrayOutputStream out = new ByteArrayOutputStream(8192);
+        ImageIO.write(image, "png", out);
+        return Base64.getEncoder().encodeToString(out.toByteArray());
+    }
+
+	private BufferedImage base64ToImage(String base64) throws IOException {
+		byte[] bytes = Base64.getDecoder().decode(base64);
+		return ImageIO.read(new ByteArrayInputStream(bytes));
+	}
+
+	private Color hexaToColor(String hexa)
 	{
-		SAXBuilder builder = new SAXBuilder();
-		Document document = new Document();
-		Element jeu = new Element("jeu");
-		document.setRootElement(jeu);
+		if (hexa.charAt(0) != '#') return null;
 
-		Element information = new Element("information");
-		jeu.addContent(information);
+		return new Color( Integer.parseInt(hexa.substring(1, 3), 16),
+		                  Integer.parseInt(hexa.substring(3, 5), 16),
+		                  Integer.parseInt(hexa.substring(5, 7), 16) );
+	}
 
-		Element dimension = new Element("dimension");
+	private String colorToHexa(Color color)
+	{
+		return "#" + Integer.toHexString(color.getRGB()).substring(2);
+	}
 
-		dimension.setAttribute("x", Integer.toString(this.imagePlateau.getWidth()));
-		dimension.setAttribute("y", Integer.toString(this.imagePlateau.getHeight()));
-		information.addContent(dimension);
+	public String toString()
+	{
+		String s = "";
+		s += "taillePlateau : " + this.taillePlateau[0] + "x " + this.taillePlateau[1] + "y\n";
+		s += "imagePlateau : " + this.imagePlateau + "\n";
+		s += "couleurPlateau : " + this.couleurPlateau + "\n";
+		s += "policePlateau : " + this.policePlateau + "\n";
+		s += "nbJoueursMin : " + this.nbJoueursMin + "\n";
+		s += "nbJoueursMax : " + this.nbJoueursMax + "\n";
+		s += "nbCarteCoul : " + this.nbCarteCoul + "\n";
+		s += "nbCarteLocomotive : " + this.nbCarteLocomotive + "\n";
+		s += "imageCarte : " + this.imageCarte + "\n";
 
-		Element image_fond = new Element("image-fond");
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		try {
-			ImageIO.write(this.imagePlateau, "png", baos);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		byte[] imageBytes = baos.toByteArray();
-		String imageString = Base64.getEncoder().encodeToString(imageBytes);
-		image_fond.setText(imageString);
-		information.addContent(image_fond);
+		return s;
+	}
+	
 
-		Element couleur_fond = new Element("couleur-fond");
-		couleur_fond.setText(Integer.toHexString(this.couleurPlateau.getRGB()).substring(2));
-		information.addContent(couleur_fond);
-
-		Element police = new Element("police");
-		police.setText(this.policePlateau.getName());
-		police.setAttribute("taille", Integer.toString(this.policePlateau.getSize()));
-		information.addContent(police);
-
-		Element image_carte = new Element("image-carte");
-		baos = new ByteArrayOutputStream();
-		try {
-			ImageIO.write(this.imageCarte, "png", baos);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		imageBytes = baos.toByteArray();
-		imageString = Base64.getEncoder().encodeToString(imageBytes);
-		image_carte.setText(imageString);
-		information.addContent(image_carte);
-
-		Element nombre_carte = new Element("nombre-carte");
-		nombre_carte.setAttribute("couleur", Integer.toString(this.nbCarteCoul));
-		information.addContent(nombre_carte);
-
-		Element nombre_joueurs = new Element("nombre-joueurs");
-		nombre_joueurs.setAttribute("min", Integer.toString(this.nbJoueursMin));
-		nombre_joueurs.setAttribute("max", Integer.toString(this.nbJoueursMax));
-		information.addContent(nombre_joueurs);
-
-		Element plateau = new Element("plateau");
-		jeu.addContent(plateau);
-
-		Element liste_objectifs = new Element("liste-objectifs");
-		plateau.addContent(liste_objectifs);
-
-
-		XMLOutputter xmlOutput = new XMLOutputter();
-		xmlOutput.setFormat(Format.getPrettyFormat());
-		try {
-			xmlOutput.output(document, new FileWriter(nomFichier));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-
-
-
+	public static void main(String[] args)
+	{
+		Metier m = new Metier("exemple");
+		System.out.println(m);
 	}
 }
