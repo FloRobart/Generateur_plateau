41238ab288ec5ca6472d26c65bc0115324384e03
Floris <Floris.robart11@gmail.com>
2023-01-03 23:43:15 +0100

    correction de bug

--- lanceur.sh
+++ lanceur.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo Lancement de l\'interface graphique...
-java -cp "./bin:$CLASSPATH" controleur.Controleur  && Fin de l'execution. || echo. & echo Veulliez compiler le projet java avant de la lancer.
+java -cp "./bin:$CLASSPATH" controleur.Controleur  && Fin de l\'execution. || echo Veulliez compiler le projet java avant de la lancer.
