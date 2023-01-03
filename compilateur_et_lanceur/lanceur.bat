bd065c075e210bb8f2a53541c16380786c01f843
Floris <Floris.robart11@gmail.com>
2023-01-03 22:36:02 +0100

    update lanceur

---
+++ compilateur_et_lanceur/lanceur.bat
@@ -0,0 +1,17 @@
+@echo off
+
+:: Création des dossiers bin et donnees s'ils n'existent pas
+IF NOT EXIST "./bin/"         ( mkdir "./bin" )
+IF NOT EXIST "./bin/donnees/" ( mkdir "./bin/donnees/" )
+
+XCOPY "./donnees" "./bin/donnees" /E /Y >NUL
+
+echo Ca compile...
+call javac -encoding utf8 "@compile.list"
+
+echo Lancement du programme...
+call java -cp ./bin;./bin/donnees;"%CLASSPATH%" controleur.Controleur
+
+echo Fin de l'execution.
+
+goto :eof
